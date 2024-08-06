31bc4b345a6583a90ca1f2dd022e134f5a1aba8c
Inal Mahpud <inalmahpud1@gmail.com>
2024-08-06 15:01:23 +0700

    optimaze seo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ContactSection from "./components/Contact";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import ProjectSection from "./components/Project";
+import { Helmet } from "react-helmet";
 
 function App() {
 useEffect(() => {
@@ -13,9 +14,14 @@ useEffect(() => {
 
   return (
     <>
-     <Navbar />
-     {/* <p className="pt-36">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus explicabo, aspernatur quia nemo minima quibusdam neque unde recusandae hic voluptates cumque quidem esse fugiat eius, dolore earum in eaque incidunt.</p>
-      */}
+      <Helmet>
+        <title>Inal Mahpud - Portfolio</title>
+        <meta
+          name="description"
+          content="Inal Mahpud's portfolio showcasing projects and skills in frontend and backend development."
+        />
+      </Helmet>
+      <Navbar />
       <HeroSection />
       <ProjectSection />
       <ContactSection />
